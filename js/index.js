cfd4a4dc7d6ee99938717b2e78ab0040353dcd24
Saulo Catunda <48536369+saulocatunda@users.noreply.github.com>
2023-02-06 20:21:37 -0300

    fix-RouteURL

--- js/index.js
+++ js/index.js
@@ -2,10 +2,13 @@ import { Router } from './routes.js'
 
 const router = new Router()
 
-router.add('/', '/pages/home.html')
-router.add('/about', '/pages/about.html')
-router.add('/explorer', '/pages/explore.html')
-router.add(404, '/pages/404.html')
+router.add('/', 'https://saulocatunda.github.io/SPA-Universe/pages/home')
+router.add('/about', 'https://saulocatunda.github.io/SPA-Universe/pages/about')
+router.add(
+  '/explorer',
+  'https://saulocatunda.github.io/SPA-Universe/pages/explore'
+)
+router.add(404, 'https://saulocatunda.github.io/SPA-Universe/pages/404')
 
 router.handle()
 
