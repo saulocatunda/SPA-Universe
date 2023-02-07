1517c14b839f1ea00a8c519a563083b52e38a234
Saulo Catunda <48536369+saulocatunda@users.noreply.github.com>
2023-02-06 21:43:27 -0300

    Fix-revertingName

--- js/index.js
+++ js/index.js
@@ -2,20 +2,17 @@ import { Router } from './routes.js'
 
 const router = new Router()
 
+router.add('/SPA-Universe/', 'https://saulocatunda.github.io/SPA-Universe/pages/home')
 router.add(
-  '/SPA-Universe/',
-  'https://saulocatunda.github.io/SPA-Universe/pages/home'
-)
-router.add(
-  '/SPA-Universe/about',
+  '/SPA-Universe/pages/about',
   'https://saulocatunda.github.io/SPA-Universe/pages/about'
 )
 router.add(
-  '/SPA-Universe/explore',
+  '/SPA-Universe/pages/explore',
   'https://saulocatunda.github.io/SPA-Universe/pages/explore'
 )
 router.add(
-  '/SPA-Universe/404',
+  '/SPA-Universe/pages/404',
   'https://saulocatunda.github.io/SPA-Universe/pages/404'
 )
 
